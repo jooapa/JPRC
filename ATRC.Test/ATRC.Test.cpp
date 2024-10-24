193a20f6370e11213585cc3238e655bff697e74a
antonako <114558368+Antonako1@users.noreply.github.com>
2024-10-24 22:25:21 +0300

    readme

--- ATRC.Test/ATRC.Test.cpp
+++ ATRC.Test/ATRC.Test.cpp
@@ -11,38 +11,11 @@ int main()
     std::shared_ptr<ATRC_FD> fd = Read(filename, "utf-8");
     fd->AutoSave = true;
     if (fd) {
-
-
-        AddBlock(fd, "test");
-        
-        AddKey(fd, "test", "troll", "aaa");
-        
-        AddKey(fd, "test", "tehi", "hi");
-        RemoveKey(fd, "test", "tehi");
-        RemoveKey(fd, "hello", "ququ");
-
-        AddKey(fd, "test", "tehi", "hi");
-        ModifyKey(fd, "test", "tehi", "helloa");
-
-        AddKey(fd, "hello", "ququ", "qaqa");
-
-        ModifyKey(fd, "hello", "ququ", "1233");
-
-        // RemoveBlock(fd, "test");
-
-        std::vector<std::string> res = atrc_to_list(';', ReadKey(fd, "moro", "hello"));
-        for(std::string &s : res){
-            std::cout << s << "\n";
-        }
-
-        res = atrc_to_list(':', ReadVariable(fd, "ScreenRatio"));
-        for(std::string &s : res){
-            std::cout << s << "\n";
-        }
-
-        bool bool_res = atrc_to_bool(ReadKey(fd, "block", "key"));
-        if(atrc_stdlib_errval == SUCCESSFULL_ACTION) 
-            std::cout << "QQ" << bool_res << "--\n" << std::endl;
+        AddBlock(fd, "block_name");
+        AddKey(fd, "block_name", "key_name", "value");
+        AddKey(fd, "block_name", "key_name2", "value");
+        RemoveKey(fd, "block_name", "key_name2");
+        ModifyKey(fd, "block_name", "key_name2", "new_value");
     } else {
         std::cerr << "Failed to read filedata." << std::endl;
     }
